01d711f403db651a054739e2e8964a392166d19d
奈須拓実 <nasutaku7nasuka@docomo.ne.jp>
2020-02-13 13:50:36 +0900

    beore-code

--- blackjack/brackjack.rb
+++ blackjack/brackjack.rb
@@ -1,26 +1,34 @@
+require './card'
+require './deck'
+require './player'
+
+# ゲームの進行
 def run_game
+  # 初期手札2枚の準備,手札公開,得点表示
   deck = Deck.new
   deck.shuffle
-  plyayer = Player.new
-  plyayer.firts_draw(deck)
-  plyayer.score_count
+  player = Player.new
+  player.first_draw(deck)
+  player.score_count
   dealer = Dealer.new
-  dealer.firts_draw(deck)
+  dealer.first_draw(deck)
   dealer.score_count
 
+  #ユーザーのターン
   while true do
     number = rand(11)
-    if number == 3 && (player.instance_variable_get :@hands).length >= 3 && player.score_count >= 11 &&plyayer.score_count != 21
-      puts "山札が輝き出した！\n山札を信じて引きますか[y/n]"
+    if number == 3 && (player.instance_variable_get :@hands).length >= 3 && player.score_count >= 11 && player.score_count != 21
+      puts "山札が輝きだした！\n山札を信じて引きますか？[Y/N]"
       puts "あなたの得点: #{player.score_count}"
       answer = gets.chomp.to_s
       if answer == "Y" || answer == "y"
-        player.setiny_draw(deck, player)
+        player.destiny_draw(deck, player)
         unless blackjack?(player)
           break
         end
       elsif answer == "N" || answer == "n"
-        puts "さらにカードを引きますか[y/n]"
+        puts
+        puts "さらにカードを引きますか？[Y/N]"
         puts "あなたの得点: #{player.score_count}"
         answer = gets.chomp.to_s
         puts "Answer: #{answer}"
@@ -38,51 +46,59 @@ def run_game
           puts "無効な値です。もう一度入力してください。"
         end
       else
-        puts "さらにカードを引きますか？[y/n]"
-        puts "あなたの得点: #{player.score_count}"
-        answer = gets.chomp.to_s
-        puts "Answer: #{answer}"
-        if answer == "Y" || answer "y"
-          player.draw(deck, player)
-          unless burst?(player)
-            exit
-          end
-          unless blackjack?(player)
-            break
-          end
-        elsif answer == "N" || answer == "n"
-          break
-        else
-          puts "無効な値です。もう一度入力してください"
+        puts "無効な値です。もう一度入力してください。"
+      end
+    else # 通常時の処理
+      puts
+      puts "さらにカードを引きますか？[Y/N]"
+      puts "あなたの得点: #{player.score_count}"
+      answer = gets.chomp.to_s
+      puts "Answer: #{answer}"
+      if answer == "Y" || answer == "y"
+        player.draw(deck, player)
+        unless burst?(player)
+          exit
         end
-      end
-    end
-
-    while true do
-      if dealer.score_count < 17
-        dealer.draw(deck, dealer)
-      else
+        unless blackjack?(player)
+          break
+        end
+      elsif answer == "N" || answer == "n"
         break
+      else
+        puts "無効な値です。もう一度入力してください。"
       end
     end
-
-    judge(player, dealer)
   end
+
+#ディーラーのターン
+  while true do
+    if dealer.score_count < 17
+      dealer.draw(deck, dealer)
+    else
+      break
+    end
+  end
+
+  judge(player, dealer)
+
 end
 
+# 勝敗判定
 def judge(player, dealer)
   player_score = player.score_count
   dealer_score = dealer.score_count
-  puts "---あなたの得点---"
+  puts
+  puts "----- あなたの得点 -----"
   puts "#{player_score}"
-  puts "---ディーラーの得点---"
+  puts "--- ディーラーの得点 ---"
   puts "#{dealer_score}"
+  puts
   if dealer.score_count == player_score
     puts "引き分け"
   elsif player_score == 21
-    puts "ブラックジャック!\nあなたの勝ちです!"
+    puts "ブラックジャック！\nあなたの勝ちです！"
   elsif dealer_score == 21
-    puts "ディーラーのブラックジャック!\nあなたの負けです..."
+    puts "ディーラーのブラックジャック！\nあなたの負けです..."
   elsif dealer_score > 21
     puts "ディーラーはバーストしました。"
     puts "あなたの勝ちです！"
@@ -93,16 +109,19 @@ def judge(player, dealer)
   end
 end
 
+# バーストしたか？
 def burst?(player)
   if player.score_count <= 21
     return true
   else
+    puts
     puts "あなたの得点: #{player.score_count}"
     puts "バーストしました。あなたの負けです。"
     return false
   end
 end
 
+# ブラックジャックかどうか
 def blackjack?(player)
   if player.score_count == 21
     return false
@@ -111,8 +130,8 @@ def blackjack?(player)
   end
 end
 
-puts "-----------------------"
+puts "---------------------"
 puts "ブラックジャックへようこそ！"
-puts "-----------------------"
-
+puts "---------------------"
+puts
 run_game
